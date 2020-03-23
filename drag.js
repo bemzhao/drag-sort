ab7c215927cdaf16ffb9383b70581e578dc0b13e
zhao <bem.zhao@foxmail.com>
2020-03-23 17:56:29 +0800

    1、300毫秒之间不可拖动 2、其他调整

--- drag.js
+++ drag.js
@@ -1,7 +1,8 @@
 class Drag {
-  constructor(parent, item) {
+  constructor(parent) {
   	this.parent = parent;													// 父容器
-  	this.item = item;															// 拖动的元素
+  	this.item = $(this.parent).children();				// 拖动的元素
+  	this.isStart = 0;															// 是否开始 0未滚动 1滚动中 2滚动完成
     this.isdrag = false;  												// 是否移动
 		this.touchY = 0;  														// 记录按下的起始位置
 		this.moveY = 0;																// 元素移动的位置
@@ -21,118 +22,129 @@ class Drag {
   }
 
   dragStart () {
-  	event.preventDefault();
+  	if (this.isStart === 0) {
+	  	event.preventDefault();
 
-  	var this_item = $(event.currentTarget);
+	  	var this_item = $(event.currentTarget);
 
-		// 记录初始位置和偏移的值
-		this.isdrag = true;
-		this.touchY = event.targetTouches[0].pageY
+			// 记录初始位置和偏移的值
+			this.isdrag = true;
+			this.isStart = 1;
+			this.touchY = event.targetTouches[0].pageY;
 
-		this.offsetY = this.touchY - this_item.offset().top;
+			this.offsetY = this.touchY - this_item.offset().top;
 
-		this_item.addClass("moving").siblings().addClass("disable");
+			this_item.addClass("moving").siblings().addClass("disable");
 
-		if (this_item.index() !== 0) {
-			this.thisItemPosTop = this_item.position().top;
+			if (this_item.index() !== 0) {
+				this.thisItemPosTop = this_item.position().top;
+			}
 		}
 	}
 
 
   dragMove () {
-		event.preventDefault();
+  	if (this.isStart === 1) {
+			event.preventDefault();
 
-		var this_item = $(event.currentTarget);
+			var this_item = $(event.currentTarget);
 
-		// 移动时产生的距离
-		this.moveY = event.targetTouches[0].pageY - this.touchY;
+			// 移动时产生的距离
+			this.moveY = event.targetTouches[0].pageY - this.touchY;
 
-		// 限制移动范围
-		var maxY = $(this.parent).outerHeight() - this_item.innerHeight()
-		if (this_item.index() === 0) {
-			// 第一个item的移动
-			// min取向下移动的最大距离，max取向上移动的最大距离
-			this.moveY = Math.min(Math.max(0, this.moveY), maxY)
-		} else {
-			// 其他item的移动
-			this.moveY = Math.min(Math.max(-this.thisItemPosTop, this.moveY), maxY-this.thisItemPosTop)
-		}
+			// 限制移动范围
+			var maxY = $(this.parent).outerHeight() - this_item.innerHeight()
+			if (this_item.index() === 0) {
+				// 第一个item的移动
+				// min取向下移动的最大距离，max取向上移动的最大距离
+				this.moveY = Math.min(Math.max(0, this.moveY), maxY)
+			} else {
+				// 其他item的移动
+				this.moveY = Math.min(Math.max(-this.thisItemPosTop, this.moveY), maxY-this.thisItemPosTop)
+			}
 
-		// 改变当前拖拽的item位置
-		if (this.isdrag === true) {
-			this_item.css({"transform": `translate3d(0, ${this.moveY}px, 0)`})
-		}
-		
-		// 判断滑动方向
-		// 移动了多少个索引加上自身的索引就是需要变化的那个索引
-		if (this.moveY > 0) {
-			this.direction = "down"
-			this.moveIndex = Math.floor((Math.abs(this.moveY)+this.offsetY)/this.itemHeight);
-			$(this.parent).children().eq(this.moveIndex+this_item.index()).not(this_item).css({
-				"transform": `translate3d(0, ${-this.itemHeight}px, 0)`,
-				"transition": ".3s"
-			})
-		} else {
-			this.direction = "up"
-			this.moveIndex = -Math.floor((Math.abs(this.moveY)+this.offsetY)/this.itemHeight);
-			$(this.parent).children().eq(this.moveIndex+this_item.index()).not(this_item).css({
-				"transform": `translate3d(0, ${this.itemHeight}px, 0)`,
-				"transition": ".3s"
-			})
-		}
-		
-		// 移动回原位时不改变位置
-		if (this.lastMoveY == this.moveY) {
-			return;
-		}
-		// 判断上滑还是下滑
-		if (this.lastMoveY > this.moveY) {
-			// console.log("向上")
-			if (this.direction === "down") {
-				$(this.parent).children().eq(this.moveIndex+this_item.index()+1).not(this_item).stop().css({
-					"transform": `translate3d(0, 0, 0)`,
+			// 改变当前拖拽的item位置
+			if (this.isdrag === true) {
+				this_item.css({"transform": `translate3d(0, ${this.moveY}px, 0)`})
+			}
+			
+			// 判断滑动方向
+			// 移动了多少个索引加上自身的索引就是需要变化的那个索引
+			if (this.moveY > 0) {
+				this.direction = "down"
+				this.moveIndex = Math.floor((Math.abs(this.moveY)+this.offsetY)/this.itemHeight);
+				$(this.parent).children().eq(this.moveIndex+this_item.index()).not(this_item).css({
+					"transform": `translate3d(0, ${-this.itemHeight}px, 0)`,
+					"transition": ".3s"
+				})
+			} else {
+				this.direction = "up"
+				this.moveIndex = -Math.floor((Math.abs(this.moveY)+this.offsetY)/this.itemHeight);
+				$(this.parent).children().eq(this.moveIndex+this_item.index()).not(this_item).css({
+					"transform": `translate3d(0, ${this.itemHeight}px, 0)`,
 					"transition": ".3s"
 				})
 			}
-		} else{
-			// console.log("向下")
-			if (this.direction === "up") {
-				$(this.parent).children().eq(this.moveIndex+this_item.index()-1).not(this_item).stop().css({
-					"transform": `translate3d(0, 0, 0)`,
-					"transition": ".3s"
-				})
+			
+			// 移动回原位时不改变位置
+			if (this.lastMoveY == this.moveY) {
+				return;
 			}
-		}
+			// 判断上滑还是下滑
+			if (this.lastMoveY > this.moveY) {
+				// console.log("向上")
+				if (this.direction === "down") {
+					$(this.parent).children().eq(this.moveIndex+this_item.index()+1).not(this_item).stop().css({
+						"transform": `translate3d(0, 0, 0)`,
+						"transition": ".3s"
+					})
+				}
+			} else{
+				// console.log("向下")
+				if (this.direction === "up") {
+					$(this.parent).children().eq(this.moveIndex+this_item.index()-1).not(this_item).stop().css({
+						"transform": `translate3d(0, 0, 0)`,
+						"transition": ".3s"
+					})
+				}
+			}
 
-		this.lastMoveY = this.moveY
+			this.lastMoveY = this.moveY
+		}
 	}
 
 
 	dragEnd () {
-		event.preventDefault();
+		if (this.isStart === 1) {
+			event.preventDefault();
 
-		var this_item = $(event.currentTarget);
+			var this_item = $(event.currentTarget);
 
-		this.isdrag = false;
-		this_item.removeClass("moving").siblings().removeClass("disable");
+			this.isStart = 2;
+			this.isdrag = false;
+			this_item.removeClass("moving").siblings().removeClass("disable");
 
-		// 设置当前的item的位置
-		this_item.css({
-			"transform": `translate3d(0, ${this.moveIndex*this.itemHeight}px, 0)`,
-			"transition": ".3s"
-		})
+			// 设置当前的item的位置
+			this_item.css({
+				"transform": `translate3d(0, ${this.moveIndex*this.itemHeight}px, 0)`,
+				"transition": ".3s"
+			})
 
-		// 用索引去更新实际位置
-		setTimeout(() => {
-			if (this.direction === "down") {
-				$(this.parent).children().eq(this.moveIndex+this_item.index()).after(this_item);
-			} else {
-				$(this.parent).children().eq(this.moveIndex+this_item.index()).before(this_item);
-			}
-			this.moveIndex = 0;
-			$(this.parent).children().attr("style", "");
-			$(this.parent).children().each((index, ele) => $(ele).find(".num").text(index+1));
-		},300)
+			// 用索引去更新实际位置
+			setTimeout(() => {
+				if (this.direction === "down") {
+					$(this.parent).children().eq(this.moveIndex+this_item.index()).after(this_item);
+				} else {
+					$(this.parent).children().eq(this.moveIndex+this_item.index()).before(this_item);
+				}
+
+				$(this.parent).children().attr("style", "");
+				$(this.parent).children().each((index, ele) => $(ele).find(".num").text(index+1));
+
+				this.moveIndex = 0;
+				this.isStart = 0;
+			},300)
+		}
 	}
-
+	
 }
