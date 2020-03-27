ade52d177d51aa0d0dbb2f4e4c8b0b6d276986da
ZhaoShaofeng <bem.zhao@foxmail.com>
2020-03-28 01:14:01 +0800

    item不定高拖拽完成

--- drag.js
+++ drag.js
@@ -10,10 +10,8 @@ class Drag {
 		this.lastMoveY = 0;  													// 记录最后移动到的位置
 		this.thisItemPosTop = 0;  										// 当前item距离父元素顶部的高度
 		this.thisItemHeight = 0; 											// 当前点击到的元素的高度
-		this.moveHeight = 0;													// 拖拽元素改变其他元素的所需要的高度
 		this.moveIndex = 0  													// 当前item移动的索引
 		this.direction = null;  											// 移动方向
-		this.isbodyScroll = false;
 		this.init();
   }
 
@@ -43,7 +41,6 @@ class Drag {
 	  	})
 
 	  	this.thisItemHeight = this_item.outerHeight();
-	  	this.moveHeight = this_parent.innerHeight()/this_parent.children().length;
 
 			// 记录初始位置和偏移的值
 			this.isdrag = true;
@@ -91,7 +88,7 @@ class Drag {
 			// 通过获取手指的移动时产生的触点来获取触碰到的元素
 			let ele = document.elementFromPoint(event.originalEvent.touches[0].pageX, event.originalEvent.touches[0].pageY);
 			if (!$(ele).hasClass(this.item.replace('.',''))) {
-				return;
+				return false;
 			}
 
 			if (this.moveY > 0 && this_item.index() < $(ele).index()) {
@@ -153,17 +150,24 @@ class Drag {
 			this.isdrag = false;
 			this_item.removeClass("moving").siblings().removeClass("disable");
 
-			//获取移动前的下标到移动后的下标之间的所有元素高度即是 tranlateY 的值
-			let mheight = this.getItemHeight(this.direction, this_item.index(), this.moveIndex+this_item.index())
-
-			if (this.moveIndex + this_item.index() > this_item.index()) {
-				// 向下滑动
+			// 计算有多少个 data-move
+			// 为 -1 就用 data-move 的个数加原有的元素本身的下标,为 1 就用原本的下标减 data-move 的个数
+			let datasetArr = [];	// 获取到的 data-move 的数组
+			let mheight = 0;			// 移动到正确位置时 translate3d 值
+			$(this.item).each((index, ele) => {
+				datasetArr.push($(ele).attr('data-move'))
+				$(ele).attr('data-move', '')
+			})
+			if (datasetArr.some((item) => {return item === "-1"})) {
+				this.moveIndex = this.getTotalInArr(datasetArr, "-1");
+				mheight = this.getItemHeight(this.direction, this_item.index(), this.moveIndex+this_item.index())
 				this_item.css({
 					"transform": `translate3d(0, ${mheight}px, 0)`,
 					"transition": ".3s"
 				})
 			} else {
-				// 向上滑动
+				this.moveIndex = this.getTotalInArr(datasetArr, "1");
+				mheight = this.getItemHeight(this.direction, this_item.index(), this_item.index()-this.moveIndex)
 				this_item.css({
 					"transform": `translate3d(0, ${-mheight}px, 0)`,
 					"transition": ".3s"
@@ -175,7 +179,7 @@ class Drag {
 				if (this.direction === "down") {
 					this_parent.children().eq(this.moveIndex+this_item.index()).after(this_item);
 				} else {
-					this_parent.children().eq(this.moveIndex+this_item.index()).before(this_item);
+					this_parent.children().eq(this_item.index()-this.moveIndex).before(this_item);
 				}
 
 				this_parent.children().attr("style", "");
@@ -187,19 +191,35 @@ class Drag {
 		}
 	}
 
-	// 获取元素原本的下标到滑到的下标的总距离
-	getItemHeight (direction, startIndex, endIndex) {
+
+	/*
+		获取元素原本的下标到滑到的下标的总距离
+		direction -> 方向
+		moveStartIndex -> 移动前的起始下标
+		moveEndIndex -> 移动结束后的下标
+	*/
+	getItemHeight (direction, moveStartIndex, moveEndIndex) {
 		let totalHeight = 0;
 		if (direction === "down") {
-			for (let i = startIndex+1; i <= endIndex; i++) {
+			for (let i = moveStartIndex+1; i <= moveEndIndex; i++) {
 				totalHeight += $(this.parent).children().eq(i).outerHeight();
 			}
 		} else {
-			for (let i = endIndex; i < startIndex; i++) {
+			for (let i = moveEndIndex; i < moveStartIndex; i++) {
 				totalHeight += $(this.parent).children().eq(i).outerHeight();
 			}
 		}
 		return totalHeight;
+	}
+
+
+	/*
+		计算某个字段在数组中出现过多少次
+		arr -> 数组
+		value -> 要计算的字段
+	*/
+	getTotalInArr (arr, value) {
+		return arr.reduce((a, v) => v === value ? a + 1 : a + 0, 0)
 	}
 	
 }
